18d0716888c70bf0e8f752122e945e587ef22cf7
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-06 21:12:39 +0500

    Create Service and interface

---
+++ Questions.UI/Aplication/Services/IQuestionServices.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using Domain.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Services
+{
+    public  interface IQuestionServices
+    {
+        Task < Response< List< Questions>>>  GetAllQuestion ( DocxFile docxFile ) ;
+        Task < Response< DocxFile>>  CreateDocxFile (  DocxFile docxFile ) ;
+        Task < Response< User>>  GetALLUser(  DocxFile docxFile ) ;
+
+    }
+}
---
+++ Questions.UI/Infrastructure/Services/IQuestionService.cs
@@ -0,0 +1,40 @@
+﻿using Aplication.Services;
+using Domain.Models;
+using Domain.Models.Response;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class IQuestionService : IQuestionServices
+    {
+        private readonly IQuestionService _questionService  ;
+        private readonly QuestionDbContext _questionDbContext ;
+
+        public IQuestionService(IQuestionService questionService, QuestionDbContext questionDbContext)
+        {
+            _questionService = questionService;
+            _questionDbContext = questionDbContext;
+        }
+
+        public Task<Response<DocxFile>> CreateDocxFile(DocxFile docxFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<Questions>>> GetAllQuestion(DocxFile docxFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<User>> GetALLUser(DocxFile docxFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
