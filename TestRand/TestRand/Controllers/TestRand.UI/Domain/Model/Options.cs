44c6386d7cdb9dde9867be34514670e190919899
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-07 11:44:41 +0500

    Create Clean code

---
+++ TestRand/TestRand/Controllers/TestRand.UI/Domain/Model/Options.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public  class Options
+    {
+        [ForeignKey("id")]
+        public int Id { get; set; }
+        public string Option { get; set; }
+        public string TrueOption { get; set; }
+
+    }
+}
---
+++ TestRand/TestRand/Controllers/TestRand.UI/Domain/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public  class User
+    {
+        public int Id { get; set; }
+        public int DocxId { get; set; }
+        public DocxFile DocxFile { get; set; } = new DocxFile();
+    }
+}
